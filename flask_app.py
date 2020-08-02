5ebb87f026f347e75c38665f526e74f85895042f
Lola <lola@jeancadene.net>
2020-08-02 10:41:20 +0200

    Add healthcheck

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 """ Basic Flask app to send a message using an url """
 from flask import Flask, request
 from messaging import send_message
+from status_code import check_status_code
 app = Flask(__name__)
 
 @app.route('/send', methods=['GET'])
@@ -16,6 +17,16 @@ def send():
     message = request.args.get('message')
     # Send the message and return the result message
     return send_message(number, message)
+
+@app.route('/healthcheck', methods=['GET'])
+def healthcheck():
+    """
+    Healthcheck. Checks the website's status code.
+    
+    Returns the website's status code.
+    """
+    
+    return check_status_code()
 
 if __name__ == "__main__":
     app.run()
---
+++ status_code.py
@@ -0,0 +1,42 @@
+""" Module to check a website's status code """
+import logging
+import os
+import configparser
+import urllib.request
+
+def check_status_code():
+    """
+    Checks the website's status code.
+
+    Returns:
+        status (int): the websites's status code
+    """
+    # Set logging level to info
+    logging.basicConfig(level=logging.INFO)
+
+    # Use config parser to get passwords from config.ini
+    CONFIG = configparser.RawConfigParser()
+
+    # Get current working directory
+    CWD = os.getcwd()
+    logging.info(f"CWD: {CWD}")
+
+    # Set path
+    CONFIG_FILE_PATH = (os.path.join(CWD, 'config.ini'))
+
+    # Get credentials for AirCard Manager
+    # Try environment variables
+    try:  
+        URL = os.environ["AIRCARD_URL"]
+    except KeyError: 
+        logging.info(f"Environment variables are not set")
+        # Get credentials from config file
+        try:
+            CONFIG.read(CONFIG_FILE_PATH)
+            URL = CONFIG.get('AirCard', 'URL')
+        except:
+            logging.error("config.ini does not exist")
+    logging.info(f"URL: {URL}")
+
+    # Return status code
+    return urllib.request.urlopen(f"{URL}/index.html").getcode()
